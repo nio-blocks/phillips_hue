e9b93f4af30cc97d80e266d2028c988d864a54e9
Scott Clay <sclay@n.io>
2018-10-10 15:26:45 -0600

    pep8

--- phillips_hue_block.py
+++ phillips_hue_block.py
@@ -1,5 +1,5 @@
 from nio.block.base import Block, Signal
-from nio.properties import VersionProperty, StringProperty, IntProperty, BoolProperty
+from nio.properties import VersionProperty, StringProperty, IntProperty
 
 import requests
 
@@ -14,7 +14,6 @@ class PhillipsHue(Block):
     hue = IntProperty(title='Hue (0-65535)', default=0)
     sat = IntProperty(title='Saturation (0-254)', default=0)
     bri = IntProperty(title='Brightness (1-254)', default=1)
-
 
     def process_signals(self, signals):
         for signal in signals:
@@ -33,5 +32,5 @@ class PhillipsHue(Block):
             x = requests.put(self.api_url, json=self.data)
             self.logger.debug(x.text)
             self.logger.debug(self.data)
-            
+
         self.notify_signals(signals)
--- tests/test_phillips_hue_block.py
+++ tests/test_phillips_hue_block.py
@@ -11,36 +11,36 @@ class TestPhillipsHue(NIOBlockTestCase):
         with patch(PhillipsHue.__module__ + '.requests') as mock_requests:
             blk = PhillipsHue()
             self.configure_block(blk, {"hub_ip": '12.12.12.12',
-                                        "user_id": '1232123ABCQ',
-                                        "light_number": 5,
-                                        "on_state": 0,
-                                        "hue": 3000,
-                                        "sat": 200,
-                                        "bri": 100})
+                                       "user_id": '1232123ABCQ',
+                                       "light_number": 5,
+                                       "on_state": 0,
+                                       "hue": 3000,
+                                       "sat": 200,
+                                       "bri": 100})
             blk.start()
             blk.process_signals([Signal({})])
             blk.stop()
             self.assert_num_signals_notified(1)
             mock_requests.put.assert_called_with(
-                'http://12.12.12.12/api/1232123ABCQ/lights/5/state', 
+                'http://12.12.12.12/api/1232123ABCQ/lights/5/state',
                 json={"on": False})
 
-    def test_on(self):        
+    def test_on(self):
         with patch(PhillipsHue.__module__ + '.requests') as mock_requests:
             blk = PhillipsHue()
             self.configure_block(blk, {"hub_ip": '12.12.12.12',
-                                        "user_id": '1232123ABCQ',
-                                        "light_number": 5,
-                                        "on_state": 1,
-                                        "hue": 3000,
-                                        "sat": 200,
-                                        "bri": 100})
+                                       "user_id": '1232123ABCQ',
+                                       "light_number": 5,
+                                       "on_state": 1,
+                                       "hue": 3000,
+                                       "sat": 200,
+                                       "bri": 100})
             blk.start()
             blk.process_signals([Signal({})])
             blk.stop()
             self.assert_num_signals_notified(1)
             mock_requests.put.assert_called_with(
-                'http://12.12.12.12/api/1232123ABCQ/lights/5/state', 
+                'http://12.12.12.12/api/1232123ABCQ/lights/5/state',
                 json={"on": True,
                       "hue": 3000,
                       "sat": 200,
