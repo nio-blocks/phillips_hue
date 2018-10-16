c774b969076b0c21742f0bd222211eb69cb385cf
Scott Clay <sclay@n.io>
2018-10-16 11:48:35 -0600

    add configurable kill switch

--- phillips_hue_block.py
+++ phillips_hue_block.py
@@ -1,14 +1,14 @@
 from nio.block.base import Block, Signal
 from nio.properties import VersionProperty, StringProperty, IntProperty, \
-                           ObjectProperty, PropertyHolder
+                           ObjectProperty, PropertyHolder, BoolProperty
 
 import requests
 
 
 class HubConfig(PropertyHolder):
 
-    hub_ip = StringProperty(title='Hub IP Address', default='[[IP]]')
-    user_id = StringProperty(title='User ID', default='[[ID]]')
+    hub_ip = StringProperty(title='Hub IP Address', default='[[HUB_IP]]')
+    user_id = StringProperty(title='User ID', default='[[USER_ID]]')
     light_number = IntProperty(title='Light Number', default=2)
 
 
@@ -16,8 +16,8 @@ class LightConfig(PropertyHolder):
 
     on_state = IntProperty(title='1 for on 0 for off', default=0)
     hue = IntProperty(title='Hue (0-65535)', default=0)
-    sat = IntProperty(title='Saturation (0-254)', default=0)
-    bri = IntProperty(title='Brightness (1-254)', default=1)
+    sat = IntProperty(title='Saturation (0-254)', default=254)
+    bri = IntProperty(title='Brightness (1-254)', default=254)
 
 
 class PhillipsHue(Block):
@@ -27,6 +27,8 @@ class PhillipsHue(Block):
         HubConfig, title='Hub Configuration', default=HubConfig())
     light_config = ObjectProperty(
         LightConfig, title='Light Configuration', default=LightConfig())
+    kill_switch = BoolProperty(title='Turn off Light at Service Stop?',
+                               default=True, advanced=True)
 
     def process_signals(self, signals):
         for signal in signals:
@@ -49,11 +51,11 @@ class PhillipsHue(Block):
         self.notify_signals(signals)
 
     def stop(self):
-        self.api_url = 'http://{0}/api/{1}/lights/{2}/state'.format(
+        if self.kill_switch:
+            self.api_url = 'http://{0}/api/{1}/lights/{2}/state'.format(
                     self.hub_config().hub_ip(),
                     self.hub_config().user_id(),
                     self.hub_config().light_number())
-        self.data["on"] = False
-        x = requests.put(self.api_url, json=self.data)
-        self.logger.debug(x.text)
+            x = requests.put(self.api_url, json={"on": False})
+            self.logger.debug(x.text)
         super().stop()
