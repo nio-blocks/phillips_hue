54bb61e24e3dd1a983ffce976c5e98bfa7549404
Scott Clay <sclay@n.io>
2018-10-16 11:07:06 -0600

    make seperate configs

--- phillips_hue_block.py
+++ phillips_hue_block.py
@@ -1,32 +1,45 @@
 from nio.block.base import Block, Signal
-from nio.properties import VersionProperty, StringProperty, IntProperty
+from nio.properties import VersionProperty, StringProperty, IntProperty, \
+                           ObjectProperty, PropertyHolder
 
 import requests
 
 
-class PhillipsHue(Block):
+class HubConfig(PropertyHolder):
 
-    version = VersionProperty('0.1.0')
     hub_ip = StringProperty(title='Hub IP Address', default='[[IP]]')
     user_id = StringProperty(title='User ID', default='[[ID]]')
     light_number = IntProperty(title='Light Number', default=2)
+
+
+class LightConfig(PropertyHolder):
+
     on_state = IntProperty(title='1 for on 0 for off', default=0)
     hue = IntProperty(title='Hue (0-65535)', default=0)
     sat = IntProperty(title='Saturation (0-254)', default=0)
     bri = IntProperty(title='Brightness (1-254)', default=1)
 
+
+class PhillipsHue(Block):
+
+    version = VersionProperty('0.1.0')
+    hub_config = ObjectProperty(
+        HubConfig, title='Hub Configuration', default=HubConfig())
+    light_config = ObjectProperty(
+        LightConfig, title='Light Configuration', default=LightConfig())
+
     def process_signals(self, signals):
         for signal in signals:
             self.data = {}
             self.api_url = 'http://{0}/api/{1}/lights/{2}/state'.format(
-                        self.hub_ip(signal),
-                        self.user_id(signal),
-                        self.light_number(signal))
-            if self.on_state(signal) == 1:
+                        self.hub_config().hub_ip(signal),
+                        self.hub_config().user_id(signal),
+                        self.hub_config().light_number(signal))
+            if self.light_config().on_state(signal) == 1:
                 self.data["on"] = True
-                self.data["hue"] = self.hue(signal)
-                self.data["sat"] = self.sat(signal)
-                self.data["bri"] = self.bri(signal)
+                self.data["hue"] = self.light_config().hue(signal)
+                self.data["sat"] = self.light_config().sat(signal)
+                self.data["bri"] = self.light_config().bri(signal)
             else:
                 self.data["on"] = False
             x = requests.put(self.api_url, json=self.data)
--- tests/test_phillips_hue_block.py
+++ tests/test_phillips_hue_block.py
@@ -10,13 +10,17 @@ class TestPhillipsHue(NIOBlockTestCase):
     def test_off(self):
         with patch(PhillipsHue.__module__ + '.requests') as mock_requests:
             blk = PhillipsHue()
-            self.configure_block(blk, {"hub_ip": '12.12.12.12',
-                                       "user_id": '1232123ABCQ',
-                                       "light_number": 5,
-                                       "on_state": 0,
-                                       "hue": 3000,
-                                       "sat": 200,
-                                       "bri": 100})
+            self.configure_block(blk, 
+                { 'hub_config': {
+                               'hub_ip': '12.12.12.12',
+                               'user_id': '1232123ABCQ',
+                               'light_number': 5},
+                'light_config': {
+                                 'on_state': 0,
+                                 'hue': 3000,
+                                 'sat': 200,
+                                 'bri': 100}
+                })
             blk.start()
             blk.process_signals([Signal({})])
             blk.stop()
@@ -28,20 +32,24 @@ class TestPhillipsHue(NIOBlockTestCase):
     def test_on(self):
         with patch(PhillipsHue.__module__ + '.requests') as mock_requests:
             blk = PhillipsHue()
-            self.configure_block(blk, {"hub_ip": '12.12.12.12',
-                                       "user_id": '1232123ABCQ',
-                                       "light_number": 5,
-                                       "on_state": 1,
-                                       "hue": 3000,
-                                       "sat": 200,
-                                       "bri": 100})
+            self.configure_block(blk,               
+                { 'hub_config': {
+                               'hub_ip': '12.12.12.12',
+                               'user_id': '1232123ABCQ',
+                               'light_number': 5},
+                'light_config': {
+                                 'on_state': 1,
+                                 'hue': 3000,
+                                 'sat': 200,
+                                 'bri': 100}
+                })
             blk.start()
             blk.process_signals([Signal({})])
             blk.stop()
             self.assert_num_signals_notified(1)
             mock_requests.put.assert_called_with(
                 'http://12.12.12.12/api/1232123ABCQ/lights/5/state',
-                json={"on": True,
-                      "hue": 3000,
-                      "sat": 200,
-                      "bri": 100})
+                json={'on': True,
+                      'hue': 3000,
+                      'sat': 200,
+                      'bri': 100})
